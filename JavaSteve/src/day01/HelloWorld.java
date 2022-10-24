328d9cd78ab08eb4bd8b8cafe111cb1ce0a29dfe
Steve <jjojjosteve@gmail.com>
2022-10-24 21:37:21 +0900

    자바 01일차: HelloWorld 기본 예제

---
+++ JavaSteve/src/day01/HelloWorld.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//콘솔에 원하는것을 입력 대신 한줄만
+		
+		/*이건 이 사이 모든것*/
+					
+		System.out.println("Hello World");
+		
+	}
+
+}
