3446393a7363602afe888ec52856f51b53968a24
Steve <jjojjosteve@gmail.com>
2022-10-24 21:37:52 +0900

    자바01일차: 변수선언

---
+++ JavaSteve/src/day01/VariableEx01.java
@@ -0,0 +1,30 @@
+package day01;
+
+public class VariableEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean isEven = false;
+		char operator = '+';
+		int number = 30;
+		float pi1 = 3.1415f; 
+		double pi2 = 3.1415;
+		//여기까지 기본자료형
+		
+		String str = "+";
+		
+		//사과의 개수를 저장하기위한 변수를 선언해보세요
+		int apple;
+		
+		//학생 평균성적을 저장하기 위한 변수를 선언해보세요
+		double scoreAvg;
+		
+		int num;
+		int NUM;
+		int num1;
+		int _num, $num;
+	
+			}
+	
+
+}
