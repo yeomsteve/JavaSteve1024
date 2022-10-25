c9ef19422fa4cfc695fe64adf6641509bd6db3a1
Steve <jjojjosteve@gmail.com>
2022-10-25 18:09:03 +0900

    자바 02일차 escape 에제

---
+++ JavaSteve/src/day02/EscapeEx01.java
@@ -0,0 +1,22 @@
+package day02;
+
+public class EscapeEx01 {
+
+	public static void main(String[] args) {
+		char ch1 = '\t';
+		char ch2 = '\n';
+		char ch3 = '\\';
+		char ch4 = '\'';
+		char ch5 = '\"';
+		char ch6 = '\u002B';
+		System.out.println(ch1 + "안녕");
+		System.out.println(ch2 + "안녕");
+		System.out.println(ch3 + "안녕");
+		System.out.println(ch4 + "안녕");
+		System.out.println(ch5 + "안녕");
+		System.out.println(ch6 + "안녕");
+		
+
+	}
+
+}
