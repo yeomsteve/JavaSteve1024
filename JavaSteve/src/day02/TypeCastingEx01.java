08f23aa3d15f6bb220dc3baa45d0d77c14bdcca1
Steve <jjojjosteve@gmail.com>
2022-10-25 18:08:29 +0900

    자바 02일차 형변환 예제

---
+++ JavaSteve/src/day02/TypeCastingEx01.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class TypeCastingEx01 {
+
+	public static void main(String[] args) {
+		float num1 = (float)1.23;
+		byte num2 = 3;
+		long num3 = 3;
+		int num4 = (int)123456678901l;
+		
+
+	}
+
+}
