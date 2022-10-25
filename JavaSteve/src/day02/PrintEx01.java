011ca89cc00dc5fccfafc35d9269bd65634520bb
Steve <jjojjosteve@gmail.com>
2022-10-25 18:08:13 +0900

    자바 02일차 콘솔출력에제

---
+++ JavaSteve/src/day02/PrintEx01.java
@@ -0,0 +1,19 @@
+package day02;
+
+public class PrintEx01 {
+
+	public static void main(String[] args) {
+		
+		String name = "Steve";
+		int age = 20;
+		System.out.println(name + "의 나이는 " + age + "살 입니다.");
+		
+		int num1 = 10;
+		int num2 = 20;
+		char operator = '+';
+		System.out.println("" + num1 + operator + num2);
+	
+	
+	}
+
+}
