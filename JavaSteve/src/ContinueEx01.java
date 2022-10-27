ccfb856d99fe0eb5c9c8ae374c603c6dfd8c94d5
Steve <jjojjosteve@gmail.com>
2022-10-27 21:38:25 +0900

    4일차 Continue 예제

---
+++ JavaSteve/src/ContinueEx01.java
@@ -0,0 +1,16 @@
+
+public class ContinueEx01 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=1;i<=5;i++) {
+			System.out.print("Hello World!");
+			if(i==3) {
+				continue;
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
