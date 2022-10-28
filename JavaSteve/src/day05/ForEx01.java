be0d8738c2802fb431b245a0d7ffad1479165c66
Steve <jjojjosteve@gmail.com>
2022-10-28 21:37:13 +0900

    05일차 FOR

---
+++ JavaSteve/src/day05/ForEx01.java
@@ -0,0 +1,19 @@
+package day05;
+
+public class ForEx01 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=1;i<=5;i++); {
+			System.out.println(i);
+		}
+		i = 5;
+		if( i%2 == 0){
+			System.out.println("짝수");			
+		}
+		else {
+			System.out.println("홀수");
+		}
+	}
+
+}
