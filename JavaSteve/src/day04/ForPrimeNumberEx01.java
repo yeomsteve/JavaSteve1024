0121ef3c04ea30f445e600d8d2d9f9055d38af4d
Steve <jjojjosteve@gmail.com>
2022-11-01 21:32:25 +0900

    edit

--- JavaSteve/src/day04/ForPrimeNumberEx01.java
+++ JavaSteve/src/day04/ForPrimeNumberEx01.java
@@ -3,7 +3,7 @@ package day04;
 public class ForPrimeNumberEx01 {
 
 	public static void main(String[] args) {
-		int num = 1, i, count;
+		int num = 2, i, count;
 		for(i=1, count=0;i<=num;i++) {
 			if(num%i==0) { count++;
 				}			
