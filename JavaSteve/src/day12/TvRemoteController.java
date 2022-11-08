d6ff7c7e40046ddd986655771e84d052dc83ff2c
Steve <jjojjosteve@gmail.com>
2022-11-08 21:42:50 +0900

    12일차 TvRemote

---
+++ JavaSteve/src/day12/TvRemoteController.java
@@ -0,0 +1,25 @@
+package day12;
+
+
+	interface TvRemoteController{
+		//전원 켜기
+		//void turnOn();
+		//전원 끄기
+		//void turnOff();
+		//전원 켜기/끄기	
+		void turn();
+		
+		//채널 변경(숫자)
+		
+		//채널 업
+		void channelUp();
+		//채널 다운
+		void channelDown();
+		//볼륨 업
+		void volumeUp();
+		//볼륨 다운
+		void volumeDown();
+		
+		void channel();
+		void print();
+}
---
+++ JavaSteve/src/day12/TvRemoteconA.java
@@ -0,0 +1,76 @@
+package day12;
+
+public class TvRemoteconA implements TvRemoteController {
+
+	private int channel;
+	private int volume;
+	private boolean power;
+	
+	@Override
+	public void turn() {
+		//power가 켜져있으면 꺼야하고, 꺼져있으면 켜야함
+		power = !power;
+}
+
+	@Override
+	public void channel(int channel) {
+		if(!power) {
+			return;
+		}
+		this.channel = channel;
+		System.out.println("채널 : " + channel);
+	}
+	
+	@Override
+	public void channelUp() {
+		if(!power) {
+			return;
+		}
+		channel++;
+		System.out.println("채널 : " + channel);
+	}
+	
+	@Override
+	public void channelDown() {
+		if(!power) {
+			return;
+		}
+		channel--;
+		channel = channel < 1 ? 999 : channel ;
+		System.out.println("채널 : " + channel);
+	}
+	
+	@Override
+	public void volumeUp() {
+		if(!power) {
+			return;
+		}
+		volume++;
+		volume = volume > 30 ? 30 : volume;
+		System.out.println("채널 : " + channel);
+	}
+	
+	@Override
+	public void volumeDown() {
+		if(!power) {
+			return;
+		}
+		volume--;
+		volume = volume < 0 ? 0 : volume;
+		System.out.println("채널 : " + channel);
+	}
+
+	@Override
+	public void print() {
+		System.out.println("전원 : " + (power ? "ON" : "OFF"));
+		System.out.println("채널 : " + channel);
+		System.out.println("볼륨 : " + volume);
+	}
+
+
+
+
+
+
+
+}
