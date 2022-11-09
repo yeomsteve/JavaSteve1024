4a53c99bb50d47e2850dcad3704c77b82c8549ce
Steve <jjojjosteve@gmail.com>
2022-11-09 21:08:09 +0900

    13일차 lombok

---
+++ JavaSteve/src/day13/LombokTest.java
@@ -0,0 +1,30 @@
+package day13;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+public class LombokTest {
+
+	public static void main(String[] args) {
+		TestA a = new TestA();
+		a.setNum(10);
+		System.out.println(a.getNum());
+		System.out.println(a);//toString()확인
+		TestA b = new TestA();
+		b.setNum(10);
+		System.out.println(a.equals(b));//equals() 확인
+	}
+}
+@Data//@Getter, @Setter, @ToString, @EqualsAndHashCode, @RequiredArgsConstructor
+class TestA{ 
+	private int num;
+
+	
+	}
+
+
+
