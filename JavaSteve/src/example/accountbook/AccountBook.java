c2d1faee0bb7252a63f4f6ff0431840da4cd642c
Steve <jjojjosteve@gmail.com>
2022-11-16 21:41:42 +0900

    18일차 AccountBook

--- JavaSteve/src/example/accountbook/AccountBook.java
+++ JavaSteve/src/example/accountbook/AccountBook.java
@@ -1,22 +1,32 @@
 package example.accountbook;
 
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class AccountBook {
+public class AccountBook{
 	ArrayList<Item> list = new ArrayList<Item>();
 	AccountService accountService = new AccountServiceImp();
 	Scanner scan;
+	
 	public void run() {
 		System.out.println("가계부 프로그램 실행합니다.");
 		int menu = -1;
+		
 		do {
-			accountService.printMenu();
-			
-			menu = scan.nextInt();
-			
-			accountService.runMenu(list, menu, scan);
-			
+			try {
+				accountService.printMenu();
+				menu = scan.nextInt();
+				accountService.runMenu(list, menu, scan);
+			}catch(ParseException e) {
+				System.out.println("날짜 형태가 잘못되었습니다.");
+			}catch(InputMismatchException e) {
+				System.out.println("잘못된 타입을 입력했습니다.");
+				scan.nextLine();
+			}catch(RuntimeException e) {
+				System.out.println(e.getMessage());
+			}
 			
 		}while(menu != 5);
 		System.out.println("가계부 프로그램 종료합니다.");
@@ -28,6 +38,5 @@ public class AccountBook {
 		}else {
 			this.scan = scan;
 		}
-
 	}
-}	
+}
--- JavaSteve/src/example/accountbook/AccountBookMain.java
+++ JavaSteve/src/example/accountbook/AccountBookMain.java
@@ -7,7 +7,5 @@ public class AccountBookMain {
 	public static void main(String[] args) {
 		AccountBook ab = new AccountBook(new Scanner(System.in));
 		ab.run();
-
 	}
-
 }
