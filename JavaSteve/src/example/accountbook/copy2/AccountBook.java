973c77d2a3e33022b320ccea095a429064f19aaa
Steve <jjojjosteve@gmail.com>
2022-11-17 21:30:21 +0900

    19일차 Accountservice by using lambda

---
+++ JavaSteve/src/example/accountbook/copy2/AccountBook.java
@@ -0,0 +1,42 @@
+package example.accountbook.copy2;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class AccountBook{
+	ArrayList<Item> list = new ArrayList<Item>();
+	AccountService accountService = new AccountServiceImp();
+	Scanner scan;
+	
+	public void run() {
+		System.out.println("가계부 프로그램 실행합니다.");
+		int menu = -1;
+		
+		do {
+			try {
+				accountService.printMenu();
+				menu = scan.nextInt();
+				accountService.runMenu(list, menu, scan);
+			}catch(ParseException e) {
+				System.out.println("날짜 형태가 잘못되었습니다.");
+			}catch(InputMismatchException e) {
+				System.out.println("잘못된 타입을 입력했습니다.");
+				scan.nextLine();
+			}catch(RuntimeException e) {
+				System.out.println(e.getMessage());
+			}
+			
+		}while(menu != 5);
+		System.out.println("가계부 프로그램 종료합니다.");
+	}
+	
+	public AccountBook(Scanner scan) {
+		if(scan == null) {
+			this.scan = new Scanner(System.in);
+		}else {
+			this.scan = scan;
+		}
+	}
+}
---
+++ JavaSteve/src/example/accountbook/copy2/AccountBookMain.java
@@ -0,0 +1,11 @@
+package example.accountbook.copy2;
+
+import java.util.Scanner;
+
+public class AccountBookMain {
+
+	public static void main(String[] args) {
+		AccountBook ab = new AccountBook(new Scanner(System.in));
+		ab.run();
+	}
+}
