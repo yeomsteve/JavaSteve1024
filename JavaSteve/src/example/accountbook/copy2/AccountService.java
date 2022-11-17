6b56de74735d4f02f9360324fe275da90d792837
Steve <jjojjosteve@gmail.com>
2022-11-17 21:30:21 +0900

    19일차 Accountservice by using lambda

--- JavaSteve/src/example/accountbook/copy2/AccountService.java
+++ JavaSteve/src/example/accountbook/copy2/AccountService.java
@@ -1,8 +1,9 @@
-package example.accountbook;
+package example.accountbook.copy2;
 
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public interface AccountService {
 	void insertItem(ArrayList<Item> list, Item item);//가계부에 있는 내역 리스트, 추가할 아이템
@@ -11,6 +12,7 @@ public interface AccountService {
 	
 	void printItem(ArrayList<Item> list, int ...days);
 	
+	void printItem(ArrayList<Item> list, Predicate<Item> p);
 	
 	boolean updateItem(ArrayList<Item> list, int index, Item item);//가계부에 있는 내역 리스트,몇번지, 수정할 아이템
 	
