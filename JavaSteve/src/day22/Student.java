100b60b087329673c8bc811f4b5d1bd6b76cdfd7
Steve <jjojjosteve@gmail.com>
2022-11-22 21:33:48 +0900

    22일차 Attendance Main

---
+++ JavaSteve/src/day22/Student.java
@@ -0,0 +1,20 @@
+package day22;
+
+import lombok.Data;
+
+@Data
+public class Student {
+	private String name, birthday;
+
+		
+	public Student(String name, String birthday) {
+		
+		this.name = name;
+		this.birthday = birthday;
+	}
+	
+	public Student(Student std) {
+		this.name = std.name;
+		this.birthday = std.birthday;
+	}
+}
